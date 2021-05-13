b9b1853613595db2e85e54474fbd547e786ec143
Nícolas Lopes Ortiz <70731847+NicolasLOrtiz@users.noreply.github.com>
2021-05-13 19:55:41 -0300

    Commit FInal

--- App.tsx
+++ App.tsx
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
+import Routes from './src/routes';
+import * as Notifications from 'expo-notifications';
+import { PlantProps } from './src/libs/storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  useEffect(() => {
+    // Escutador de notificação
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      }
+    );
+
+    // return () => subscription.remove();
+
+    // Cancelar notificações
+    // async function notifications() {
+    //   await Notifications.cancelAllScheduledNotificationsAsync();
+
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("#### Notificções Agendadas ####");
+    //   console.log(data);
+    // }
+
+    // notifications();
+  }, []);
+
+  if(!fontsLoaded) {
+    return (
+      <AppLoading />
+    );
+  }
+
+  return(
+    <Routes />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
